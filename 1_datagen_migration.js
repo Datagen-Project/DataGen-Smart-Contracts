72a3d921ab92643b4f7184c8270e0fc29c3c812c
William <kan881271@gmail.com>
2021-08-16 09:51:03 -0400

    Reserved Pool Contract

--- 1_datagen_migration.js
+++ 1_datagen_migration.js
@@ -1,9 +1,14 @@
 const DataGen = artifacts.require("DataGen");
 const RetailPrivateSale = artifacts.require("RetailPrivateSale");
 const VCPrivateSale = artifacts.require("VCPrivateSale");
+const ReservedPool = artifacts.require("ReservedPool");
 
-module.exports = function (deployer) {
-  deployer.deploy(DataGen);
-  deployer.deploy(RetailPrivateSale);
-  deployer.deploy(VCPrivateSale);
+//Need to change when the contract is being deployed.
+const companyWallet = "";
+
+module.exports = async function (deployer) {
+  await deployer.deploy(DataGen);
+  await deployer.deploy(RetailPrivateSale);
+  await deployer.deploy(VCPrivateSale);
+  await deployer.deploy(ReservedPool, DataGen.address, companyWallet);
 };
