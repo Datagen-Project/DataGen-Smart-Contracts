dd60c5090349434b36961f247c2ec785abc228a8
Luca Eugenio <88165605+Lord-Nymphis@users.noreply.github.com>
2021-08-19 11:41:38 +0200

    Update DataGen.sol

--- DataGen.sol
+++ DataGen.sol
@@ -49,11 +49,7 @@ contract DataGen is Ownable, IERC20 {
     /**
      * @dev Returns the number of decimals used to get its user representation.
      * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 20, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless this function is
-     * overloaded;
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`)
      *
      * NOTE: This information is only used for _display_ purposes: it in
      * no way affects any of the arithmetic of the contract, including
