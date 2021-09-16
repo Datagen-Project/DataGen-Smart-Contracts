09cdcd2ae85b2dcd9d6aeb5fd82691c0dca229b3
viac92 <64146594+viac92@users.noreply.github.com>
2021-09-16 18:11:40 +0200

    adding vc and retail to migration

--- migration/1_datagen_migration.js
+++ migration/1_datagen_migration.js
@@ -1,16 +1,26 @@
-const DataGen = artifacts.require("DataGen");
-const RetailPrivateSale = artifacts.require("RetailPrivateSale");
-const VCPrivateSale = artifacts.require("VCPrivateSale");
-const ReservedPool = artifacts.require("ReservedPool");
-const CoFounderPool = artifacts.require("CoFounderPool");
-const TeamMainPool = artifacts.require("TeamMainPool");
-const TeamBonusPool = artifacts.require("TeamBonusPool");
+const DataGen = artifacts.require("./DataGen.sol");
+const RetailPrivateSale = artifacts.require("./RetailPrivateSale.sol");
+const VCPrivateSale = artifacts.require("./VCPrivateSale.sol");
+const ReservedPool = artifacts.require("./ReservedPool.sol");
+const CoFounderPool = artifacts.require("./CoFounderPool.sol");
+const TeamMainPool = artifacts.require("./TeamMainPool.sol");
+const TeamBonusPool = artifacts.require("./TeamBonusPool.sol");
 
 //Need to change when the contract is being deployed.
-const companyWallet = "";
+const companyWallet = "0x2A5f12b2F128cE1b394201B9A24Ca257af25720F";
 //Angela & Luca's wallet
-const aWallet = "";
-const lWallet = "";
+const aWallet = "0x02F1283563545EefCa20578358A650DE84e9f46b";
+const lWallet = "0x29Da73Bc911089059841a37927Ccd2e2A656D084";
+
+//Set time to the VC contract
+const VCStartTime = '1631806094';
+const VCEndTime = '163180694';
+const VCLockTime = '1631816094';
+
+//Set time to Retail contract
+const RetailStartTime = '1631806094';
+const RetailEndTime = '163180694';
+
 
 module.exports = async function (deployer) {
   await deployer.deploy(DataGen);
@@ -18,8 +28,8 @@ module.exports = async function (deployer) {
   const deployedBlock = await web3.eth.getBlock(transaction.blockNumber);
   const deployedTime = deployedBlock.timestamp;
     
-  await deployer.deploy(RetailPrivateSale);
-  await deployer.deploy(VCPrivateSale);
+  await deployer.deploy(RetailPrivateSale, DataGen.address, VCStartTime, VCEndTime, VCLockTime);
+  await deployer.deploy(VCPrivateSale, DataGen.address, RetailStartTime, RetailEndTime);
   await deployer.deploy(ReservedPool, DataGen.address, companyWallet);
   await deployer.deploy(CoFounderPool, DataGen.address, aWallet, lWallet, deployedTime);
   await deployer.deploy(TeamMainPool, DataGen.address);
