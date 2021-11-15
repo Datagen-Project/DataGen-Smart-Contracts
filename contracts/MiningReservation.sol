5b482bd8d08e70c7b54a54074c228d5f706e7429
joeheror <peter.jas0n325@gmail.com>
2021-11-15 21:09:57 +0900

    update miningreservation

--- contracts/MiningReservation.sol
+++ contracts/MiningReservation.sol
@@ -18,6 +18,23 @@ contract MiningReservation is Ownable, ReentrancyGuard {
   /* Available at first */
   uint256 public startAmount = 2280 * (10**18);
   uint256 public beginAmount = 2280 * (10**18);
+
+  /* Votation time*/
+  uint256 public votationStartTime = 0;
+  uint256 public votationDuration = 0;
+
+  /* votation staking*/
+  mapping( address => uint256 ) stakeAmount;
+  mapping( uint256 => address ) stakers;
+  uint256 public totalStakeAmount;
+  uint256 public stakerCount = 0;
+
+  /* votation info */
+  mapping( address => uint256 ) voteInfo;
+  mapping( uint256 => uint256 ) totalVoteInfo;
+  mapping( uint256 => uint256 ) totalVotedDG;
+  uint256 public voteOption;
+
   /* the address of the token contract */
 	IERC20 public dataGen;
 
@@ -26,26 +43,97 @@ contract MiningReservation is Ownable, ReentrancyGuard {
 
   event SetMiningWalletAddress(address indexed user, address indexed miningWallet); 
 
+
+  modifier duringVotation() {
+    require( votationStartTime > 0, "votation start time is not set");
+    require( votationDuration > 0, "votation duration is not set");
+    require( block.timestamp >= votationStartTime, "votation is not started");
+    require( block.timestamp <= votationStartTime + votationDuration, "votation ended");
+    _;
+  }
+  
+   modifier afterVotation() {
+    require( votationStartTime > 0, "votation start time is not set");
+    require( votationDuration > 0, "votation duration is not set");
+    require( block.timestamp >= votationStartTime + votationDuration, "votation not ended");
+    _;
+  }
   /*  initialization, set the token address */
   constructor(IERC20 _dataGen) {
     dataGen = _dataGen;
   }
 
-  function setMiningWallet(address _miningWallet) external onlyOwner {
+  function setMiningWallet(address _miningWallet) external onlyOwner afterVotation {
     miningWallet = _miningWallet;
     emit SetMiningWalletAddress(msg.sender, miningWallet);
+  }
+
+  function stake( uint256 amount ) external duringVotation nonReentrant{
+    require( dataGen.balanceOf(msg.sender) >= amount, "you have not enough #DG to stake");
+    require( voteInfo[msg.sender] == 0, "you can't stake after vote");
+
+    if( stakeAmount[msg.sender] == 0 ) {
+      stakers[stakerCount] = msg.sender;
+      stakerCount++;
+    }
+    dataGen.transferFrom(msg.sender, address(this), amount);
+    stakeAmount[msg.sender] += amount;
+    totalStakeAmount += amount;
+
+  }
+
+  function vote( uint256 position ) external duringVotation nonReentrant {
+    require( stakeAmount[msg.sender] > 0, "you must stake before vote");
+    require( voteInfo[msg.sender] == 0, "you already voted");
+    require( position > 0, "vote position must be bigger than 0");
+    require( position <= voteOption, "position must be less than total vote option count");
+
+    voteInfo[msg.sender] = position;
+    totalVoteInfo[position]++;
+    totalVotedDG[position] += stakeAmount[msg.sender];
+  }
+
+  function getVoteInfo( uint256 position ) external view returns(uint256) {
+    require( position > 0, "position must be bigger than 0");
+    require( position <= voteOption, "position must be less than total vote option count");
+    return totalVoteInfo[position];
+  }
+
+  function setVotationInfo( uint256 startTime, uint256 duration, uint256 totalOption ) external onlyOwner {
+    require(startTime > 0, "vote start time must be bigger than 0");
+    require( duration > 0, "vote duration must be bigger than 0");
+    require( totalOption > 0, "option count must be bigger than 0");
+    votationStartTime = startTime;
+    votationDuration = duration;
+    voteOption = totalOption;
+  }
+
+  function getWinner() external onlyOwner returns(uint256) {
+    uint256 winnerDGCount = 0;
+    uint256 winnerInfo;
+    for( uint256 i = 1; i <= voteOption; i++ ) {
+      if( winnerDGCount < totalVotedDG[i] ) {
+        winnerDGCount = totalVotedDG[i];
+        winnerInfo = i;
+      }
+    }
+    for( uint256 i = 0; i < stakerCount ; i++ ) {
+      address stakerAddr = stakers[i];
+      dataGen.transfer(stakerAddr, stakeAmount[stakerAddr]);
+    }
+    return winnerInfo;
   }
 
   function releaseDataGen() public nonReentrant {
     require(dataGen.balanceOf(address(this)) > 0, "Zero #DG left.");
     require(block.timestamp >= lockTime, "Still locked.");
-
+    
     uint256 balance = dataGen.balanceOf(address(this));
 
     uint256 plusDate = multipler.mul(1095).sub(1095); 
     uint256 epochs = block.timestamp.sub(lockTime).div(24 * 3600).add(1).sub(plusDate);
     if (epochs > 1095) {
-      epochs = 1;
+      epochs = epochs - 1095;
       multipler ++;
       beginAmount = beginAmount.div(2);
     }
@@ -58,7 +146,7 @@ contract MiningReservation is Ownable, ReentrancyGuard {
       mintUnitAmount = mintUnitAmount.div(2);
       counter--;
     }
-    releaseAmount = beginAmount.mul(epochs);
+    releaseAmount = beginAmount.mul(epochs).add(releaseAmount);
     uint256 leftAmount = totalLocked.sub(releaseAmount);
 
     require(balance > leftAmount, "Already released.");
