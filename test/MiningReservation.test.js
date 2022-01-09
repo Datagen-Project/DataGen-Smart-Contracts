19047e00a76d51161ccfe5d13eb37f455ec2b65c
viac92 <64146594+viac92@users.noreply.github.com>
2022-01-09 17:24:11 +0100

    add test initial attributes

--- test/MiningReservation.test.js
+++ test/MiningReservation.test.js
@@ -17,13 +17,31 @@ contract("MiningReservation", accounts => {
         this.USDCToken = await USDC.new();
 
         this.MiningReservation = await MiningReservation.new(this.DatagenToken.address);
+
+        const fundDG = new BN("15000000000000000000000000");
+        await this.DatagenToken.transfer(this.MiningReservation.address, fundDG, {from: accounts[0]});
     });
 
     describe("Initial attributes", function() {
-        it("Has the correct lockTime", async function() {
+        it("has the correct lockTime", async function() {
             const lockTime = await this.MiningReservation.lockTime();
 
-            lockTime.toString().should.equal("1704067200");
-        }); 
+            lockTime.toString().should.equal("1711922400");
+        });
+        it("has the correct totalLocked amount of DG", async function() {
+            const totalLocked = await this.MiningReservation.totalLocked();
+
+            totalLocked.toString().should.equal("15000000000000000000000000");
+        });
+        it("has the correct initial multipler = 1", async function() {
+            const multipler = await this.MiningReservation.multipler();
+
+            multipler.toString().should.equal("1");
+        });
+        it("has the correct minReleaseRate", async function() {
+            const minReleaseRate = await this.MiningReservation.minReleaseRate();
+
+            minReleaseRate.toString().should.equal("71250000000000000000");
+        });
     });
 });
